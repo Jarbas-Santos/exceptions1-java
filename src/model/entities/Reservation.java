fc0d7f8b3bc1ea2744c48c24348ecbf5abde0c69
Jarbas Santos <sjarbas333@gmail.com>
2022-07-13 23:56:32 -0300

    Very bad solution

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,68 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;	
+	
+	// Para que não seja instanciado um novo SimpleDateFormat para cada objeto reservation, 
+	// deixo esse método estático.
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation() {
+	}
+	
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	public long duration() {
+		// getTime(), me devolve a quantidade de milissegundos dessa data.
+		// Aqui calculo a diferença entre essas datas em milissegundos.
+		long diff = checkOut.getTime() - checkIn.getTime();
+		// Agora converto isso em dias. Chamando a classe TimeUnit. 
+		// Ela é um tipo enumerado complexo que tem algumas operações.
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room "
+				+ roomNumber
+				+ ", check-in: "
+				+ sdf.format(checkIn)
+				+ ", check-out: "
+				+ sdf.format(checkOut)
+				+ ", "
+				+ duration()
+				+ " nights";
+	}
+}
